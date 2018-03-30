0b511054e881bb27dfeee4413d8c7f3962c3c66b
Bardan Rana <bardan.rana@gmail.com>
2018-03-30 16:32:52 +0545

    Create bi.js

---
+++ bi.js
@@ -0,0 +1,9 @@
+ele = document.getElementsByName('loginfmt')[0];
+m = ko.contextFor(ele);
+m.$parentContext.$parent.usernameTextbox.value('delivery@cloudfactory.com’);
+ele = document.getElementById(‘i0281';)
+m = ko.contextFor(ele);
+document.getElementById('idSIButton9').click();
+pass=ko.contextFor(document.getElementsByName('passwd')[0]);
+pass.$parentContext.$parent.passwordTextbox.value(‘<password here>’);
+document.getElementById('idSIButton9').click();
